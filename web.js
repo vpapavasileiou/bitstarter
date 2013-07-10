2d4748c7877ebe87ef8c7b18a46f3937e0cf9160
Ubuntu <ubuntu@ip-172-31-17-201.us-west-2.compute.internal>
2013-07-10 22:21:33 +0000

    the buf version

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html'));
+
+var stringfile = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(stringfile);
 });
 
 var port = process.env.PORT || 5000;
