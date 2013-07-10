5f7ada1dcea65c6e7d7070f6ddbab6a057648db8
Ubuntu <ubuntu@ip-172-31-17-201.us-west-2.compute.internal>
2013-07-10 22:57:16 +0000

    the buf version

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
+
 var app = express.createServer(express.logger());
-var fs =require('fs');
-var buff = new Buffer(); 
 
 app.get('/', function(request, response) {
-  response.send(buff.toString('utf-8', fs.readFileSync("index.html")))
- });
+var fs = require ('fs');
+var buffer = new Buffer(256);
+  response.send(fs.readFileSync('index.html').toString()); 
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
