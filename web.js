0c7efd5619e5471708e41d775cb35f9158b57c32
Ubuntu <ubuntu@ip-172-31-17-201.us-west-2.compute.internal>
2013-07-10 22:41:11 +0000

    the buf version

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
 var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+  response.send(buffer.toString('utf-8', fs.readFileSync("index.html")))});
  });
 
 var port = process.env.PORT || 5000;
